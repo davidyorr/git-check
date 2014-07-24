d6d6d2c30655b22576df9c7153d6bdf088d044ca
davidyorr <davidyorr@gmail.com>
2014-07-23 21:30:36 -0400

    set up to run extra git commands. the overridden main git command will call the original.

--- _git_print_check.d/gpc.sh
+++ _git_print_check.d/gpc.sh
@@ -5,5 +5,11 @@ source ~/.git_print_check.d/gpc.conf
 
 # override the git command
 function git {
+  command=$1
 
+  if [ $command == "check" ]; then
+    :
+  else
+    "`which git`" "$@"
+  fi
 }
