fba2ddcde71d893579606096ac27a45ec718e7f4
davidyorr <davidyorr@gmail.com>
2014-07-24 23:09:21 -0400

    git check command says how many print statements were found

--- _git_print_check.d/gpc.sh
+++ _git_print_check.d/gpc.sh
@@ -5,11 +5,27 @@ source ~/.git_print_check.d/gpc.conf
 
 # override the git command
 function git {
-  command=$1
+  local command=$1
+  local gitCommand=`which git`
 
   if [ $command == "check" ]; then
-    :
+    __gpc_git_check
   else
-    "`which git`" "$@"
+    "$gitCommand" "$@"
   fi
+}
+
+# check for print statements that are being introduced
+function __gpc_git_check {
+  local totalAmount=0
+  local s
+  for s in "${PRINT_STATEMENTS[@]}"; do
+    local printStatement=$("$gitCommand" diff | grep "$s")
+    if [ ${#printStatement} == 0 ]; then
+      continue
+    fi
+    local amount=$(echo "$printStatement" | wc -l)
+    totalAmount=$((totalAmount + amount))
+  done
+  printf 'Found %d print statement%s\n' $totalAmount $([ "$totalAmount" != 1 ] && echo 's')
 }
