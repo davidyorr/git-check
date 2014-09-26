c72b4f7537b7e89dd60e6a65becf686434b594a0
davidyorr <davidyorr@gmail.com>
2014-09-25 23:08:36 -0400

    cleaned installer. does nothing for now.

--- gitcheck-installer.sh
+++ gitcheck-installer.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# install.sh
 
-# move .git_print_check.d
-printf "\nCreating ~/.git_print_check.d directory\n"
-cp -R _git_print_check.d ~/.git_print_check.d
-
-printf "Adding gpc.sh to ~/.bashrc\n"
-cat _bashrc >> ~/.bashrc
-
-printf "Done\n\n"
+# git-check installer for *nix systems
+echo "# git-check installer #"
+echo ""
